8464daf9a7c2cc3e0070084e2ab7181573a5b74a
EmilioColds <emiliofriastapia2705@outlook.com>
2024-04-11 16:06:05 -0600

    Added an initial code to main.handlebars and server.js

--- server.js
+++ server.js
@@ -2,3 +2,24 @@ const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars').engine;
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+//////////////////////////////////////////////////
+// Código de branch Handlebars para visualizar
+
+const app = express();
+const PORT = 3001;
+
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.render('main', { layout : 'main' });
+});
+
+app.listen(PORT, () => {
+    console.log(`Servidor escuchando en http://localhost:${PORT}`);
+});
+
+/////////////////////////////////////////////////////////
