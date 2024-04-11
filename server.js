70dc763910dc88b40606b8362eb3422ba3e47547
EmilioColds <emiliofriastapia2705@outlook.com>
2024-04-11 16:53:21 -0600

    Finished with the main styles

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 // Código de branch Handlebars para visualizar
 
 const app = express();
-const PORT = 3001;
+const PORT = 3003;
 
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
@@ -15,7 +15,7 @@ app.set('view engine', 'handlebars');
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-    res.render('main', { layout : 'main' });
+    res.render('homepage', { layout : 'main' });
 });
 
 app.listen(PORT, () => {
