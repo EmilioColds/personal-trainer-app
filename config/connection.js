73a07437814a454b0bfbabd722a6f2620ad4221b
MARTIN VIVANCO <martinvivanco@live.com.mx>
2024-04-11 19:53:10 -0600

    CONFIG/SERVER LISTO

---
+++ config/connection.js
@@ -0,0 +1,19 @@
+const { Sequelize } = require('sequelize');
+require('dotenv').config();
+
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+    host: 'localhost',
+    dialect: 'mysql',
+});
+
+
+async function conectarBaseDeDatos() {
+    try {
+        await sequelize.authenticate();
+        console.log('Conexión a la base de datos establecida correctamente.');
+    } catch (error) {
+        console.error('Error al conectar con la base de datos:', error);
+    }
+}
+
+module.exports = { sequelize, conectarBaseDeDatos };
--- server.js
+++ server.js
@@ -1,4 +1,40 @@
 const express = require('express');
 const session = require('express-session');
-const exphbs = require('express-handlebars').engine;
+const exphbs = require('express-handlebars');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const { sequelize, conectarBaseDeDatos } = require('./config/connection');
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+
+const sessionStore = new SequelizeStore({
+    db: sequelize
+});
+
+
+sessionStore.sync();
+
+
+app.use(express.static('public'));
+
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+conectarBaseDeDatos()
+    .then(() => {
+        app.listen(PORT, () => {
+            console.log(`Servidor iniciado en el puerto ${PORT}`);
+        });
+    })
+    .catch(error => {
+        console.error('Error al conectar con la base de datos:', error);
+    });
+
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
