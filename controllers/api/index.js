e320d6f1ad00634503a5623ff53ac53573a077cf
carlos-roma <adrianc.rm0@gmail.com>
2024-04-17 19:53:27 -0600

    ultimos fixes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
-const trainingRoutes = require('./trainingRoutes');
+const trainingRoutes = require('./trainningroutes');
 
 router.use('/users', userRoutes);
 router.use("/training", trainingRoutes);
